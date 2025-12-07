d66e5e166aef39070688551025522dbc9d31d294
MCPika <merwann.charpentier@gmail.com>
2025-12-07 13:54:51 +0100

    v1.6
    Updated the Breeding Tree with no expanding tree bugs

--- app/src/main/java/com/example/palcompanion/model/Filter.kt
+++
@@ -1,8 +0,0 @@
-package com.example.palcompanion.model
-
-data class Filter(
-    val name: String,
-    val iconUrl: String,
-    val workSuitability: WorkSuitability? = null,
-    val palElement: PalElement? = null
-)
